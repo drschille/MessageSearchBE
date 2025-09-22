7fa542dbc3a128d0b43f3db2dc9ed59b33c24f02
Daniel Robert Schille <13994573+drschille@users.noreply.github.com>
2025-09-22 22:57:04 +0200

    Fix typo in import statement for calllogging plugin in Server.kt

--- app/src/main/kotlin/org/themessagesearch/app/Server.kt
+++ app/src/main/kotlin/org/themessagesearch/app/Server.kt
@@ -7,7 +7,7 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.calllogging.*
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
